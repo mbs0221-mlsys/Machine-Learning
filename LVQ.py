abb92af32a01d39583e83373cc35dfa8ed8bc081
mbs0221 <1019329461@qq.com>
2018-01-11 23:26:57 +0800

    实现了LVQ学习向量量化算法

--- LVQ.py
+++ LVQ.py
@@ -7,7 +7,7 @@ category_list = [0, 0, 0, 0, 1, 1, 1, 1]  # 对应着输入节点的类别（两
 category = 2
 
 
-class LVQ():
+class LVQ:
     """
     Learning Vector Quantization
 
@@ -18,30 +18,30 @@ class LVQ():
     """
 
     def __init__(self, category):
-        self.input_layer = input_layer  # 输入样本
-        self.output_layer = []  # 输出数据
-        self.output_layer_cate = []  # 存储着输出样本的种类
+        self.input = input_layer  # 输入样本
+        self.output = []  # 输出数据
+        self.output_cate = []  # 存储着输出样本的种类
         self.step_alpha = 0.5  # 步长 初始化为0.5
         self.step_alpha_del_rate = 0.95  # 步长衰变率
         self.category = category  # 类别个数
         self.category_list = category_list
-        self.output_layer_length = len(self.input_layer[0])  # 输出样本特征个数 2
+        self.output_length = len(self.input[0])  # 输出样本特征个数 2
         self.d = [0.0] * self.category
 
     # 初始化 output_layer
-    def initial_output_layer(self):
+    def initial_output(self):
         for i in range(self.category):
-            self.output_layer_cate.append(i)  # 主要分为两类 0/1 。初始化输出类别列表,分别对应着两个输出节点
-            self.output_layer.append([])
-            for _ in range(self.output_layer_length):
-                self.output_layer[i].append(random.randint(0, 400))
+            self.output_cate.append(i)  # 主要分为两类 0/1 。初始化输出类别列表,分别对应着两个输出节点
+            self.output.append([])
+            for _ in range(self.output_length):
+                self.output[i].append(random.randint(0, 400))
 
     # lvq 算法的主要逻辑
     # 计算某个输入样本 与 所有的输出节点之间的距离,存储于 self.d 之中
     def calc_distance(self, a_input):
         self.d = [0.0] * self.category
         for i in range(self.category):
-            w = self.output_layer[i]
+            w = self.output[i]
             # self.d[i] =
             for j in range(len(a_input)):
                 self.d[i] += math.pow((a_input[j] - w[j]), 2)  # 就不开根号了
@@ -55,18 +55,18 @@ class LVQ():
     # 将输出节点朝着当前的节点逼近或远离
     def move(self, a_input_index, a_input, min_output_index):
         # 作为有监督式学习 这里体现了与som算法不同之处（也是唯一的不同之处）
-        if self.category_list[a_input_index] == self.output_layer_cate[min_output_index]:
-            for i in range(len(self.output_layer[min_output_index])):
-                self.output_layer[min_output_index][i] = self.output_layer[min_output_index][i] + self.step_alpha * (a_input[i] - self.output_layer[min_output_index][i])
+        if self.category_list[a_input_index] == self.output_cate[min_output_index]:
+            for i in range(len(self.output[min_output_index])):
+                self.output[min_output_index][i] = self.output[min_output_index][i] + self.step_alpha * (a_input[i] - self.output[min_output_index][i])
         else:
-            for i in range(len(self.output_layer[min_output_index])):
-                self.output_layer[min_output_index][i] = self.output_layer[min_output_index][i] - self.step_alpha * (a_input[i] - self.output_layer[min_output_index][i])
+            for i in range(len(self.output[min_output_index])):
+                self.output[min_output_index][i] = self.output[min_output_index][i] - self.step_alpha * (a_input[i] - self.output[min_output_index][i])
 
     # lvq 主要逻辑 (一次循环)
     def train(self):
         # for a_input in self.input_layer:
-        for i in range(len(self.input_layer)):
-            a_input = self.input_layer[i]
+        for i in range(len(self.input)):
+            a_input = self.input[i]
             self.calc_distance(a_input)
             min_output_index = self.get_min(self.d)
             self.move(i, a_input, min_output_index)
@@ -76,7 +76,7 @@ class LVQ():
         generate = 0
         text = "代数:{0} 此时步长:{1} 输出节点:{2} 对应的类别为:{3}"
         while self.step_alpha >= 0.0001:  # 这样子会执行167代
-            print(text.format(generate, self.step_alpha, self.output_layer, self.output_layer_cate))
+            print(text.format(generate, self.step_alpha, self.output, self.output_cate))
             self.train()
             generate += 1
             self.step_alpha *= self.step_alpha_del_rate  # 步长衰减
@@ -84,5 +84,5 @@ class LVQ():
 
 if __name__ == '__main__':
     lvq = LVQ(category)
-    lvq.initial_output_layer()
+    lvq.initial_output()
     lvq.loop()
